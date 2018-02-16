55db9b18770f4faad19c0e54e6f6cf7a30963bb1
20132219@aloe.ulima.edu.pe <20132219@aloe.ulima.edu.pe>
2018-02-16 13:59:16 -0500

    base2

--- mundialitisapps/urls.py
+++ mundialitisapps/urls.py
@@ -20,5 +20,16 @@ urlpatterns = [
     #url(r'^2/', views.index3, name='index3')
 
     url(r'^details/(?P<id>\d+)-(?P<ttlscore>\d+)/$', views.details, name='details'),
-    url(r'^processing/(?P<option>[-\w]+)-(?P<id>\d+)-(?P<ttlscore>\d+)/$', views.processing, name='processing')
+    url(r'^processing/(?P<option>[-\w]+)-(?P<id>\d+)-(?P<ttlscore>\d+)/$', views.processing, name='processing'),
+
+    #invitaciones
+    url(r'^invitar/', views.invitar, name='invitar'),
+    url(r'^perfil/', views.perfil, name='perfil'),
+    url(r'^invitarusuario/', views.invitarusuario, name='invitarusuario'),
+    url(r'^agregargrupo/', views.agregargrupo, name='agregargrupo'),
+    url(r'^responderinvitacion/', views.responderinvitacion, name='responderinvitacion'),
+    url(r'^useradmingroup/', views.useradmingroup, name='useradmingroup'),
+    url(r'^misgrupos/', views.misgrupos, name='misgrupos'),
+    url(r'^grupousuarios/', views.grupousuarios, name='grupousuarios'),
+    url(r'^usuariosgrupo/', views.usuariosgrupo, name='usuariosgrupo')
 ];
